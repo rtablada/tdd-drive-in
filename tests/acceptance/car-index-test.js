5575d297dfff8349f1835d0a192076710522a5b0
Ryan Tablada <ryan.tablada@gmail.com>
2017-10-05 11:43:09 -0500

    Updated tests

--- tests/acceptance/car-index-test.js
+++ tests/acceptance/car-index-test.js
@@ -1,4 +1,5 @@
 import { test } from 'qunit';
+import { faker } from 'ember-cli-mirage';
 import moduleForAcceptance from 'tdd-drive-in/tests/helpers/module-for-acceptance';
 
 moduleForAcceptance('Acceptance | cars index');
@@ -18,7 +19,7 @@ test('user can navigate to the cars index', async (assert) => {
 });
 
 test('user can see all cars from the API', async (assert) => {
-  server.createList('car', 20);
+  server.createList('car', faker.random.number({ min: 8, max: 20 }));
 
   await visit('/cars');
 
@@ -28,9 +29,14 @@ test('user can see all cars from the API', async (assert) => {
   const carSeries = findWithAssert('[data-test-car-series]').toArray();
   const carYear = findWithAssert('[data-test-car-year]').toArray();
 
-  assert.equal(carItems.length, server.db.cars.length);
-  assert.deepEqual(carBrands.map(c => c.innerText), server.db.cars.map(c => c.brand));
-  assert.deepEqual(carColors.map(c => c.innerText), server.db.cars.map(c => c.color));
-  assert.deepEqual(carSeries.map(c => c.innerText), server.db.cars.map(c => c.series));
-  assert.deepEqual(carYear.map(c => c.innerText), server.db.cars.map(c => c.year));
+  assert.equal(carItems.length, server.db.cars.length,
+    'There should be a car item for each car in the API');
+  assert.deepEqual(carBrands.map(c => c.innerText), server.db.cars.map(c => c.brand),
+    'The brands for each car UI should match the brand values in the API database.');
+  assert.deepEqual(carColors.map(c => c.innerText), server.db.cars.map(c => c.color),
+    'The brands for each car UI should match the brand values in the API database.');
+  assert.deepEqual(carSeries.map(c => c.innerText), server.db.cars.map(c => c.series),
+    'The brands for each car UI should match the brand values in the API database.');
+  assert.deepEqual(carYear.map(c => c.innerText), server.db.cars.map(c => c.year),
+    'The brands for each car UI should match the brand values in the API database.');
 });
